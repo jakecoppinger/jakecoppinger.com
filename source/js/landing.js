cc9bf0fa010f57687c961cb9eadcf5ca2d747fd6
Jake Coppinger <jake@jakecoppinger.com>
2016-06-18 22:05:54 +1000

    All images on landing working

--- source/js/landing.js
+++ source/js/landing.js
@@ -1,22 +1,23 @@
 $(document).ready(function() {
-    registerBackgroundImage("departing");
-    registerBackgroundImage("robot");
+    ['transit-in-sydney', 'departing',
+        'swirlesque', 'engineering-process', 'films'
+    ].forEach(function(imageName) {
+        $("#landingscreen a." + imageName).hover(
+            function() {
+
+                $("#maintitle").css("opacity", "1");
+                //$("#maintitle").css("mix-blend-mode", "exclusion");
+                $(".landingimage." + imageName).stop().fadeTo(200, 1);
+
+            },
+            function() {
+
+                $("#maintitle").css("opacity", "1");
+                //$("#maintitle").css("mix-blend-mode", "normal");
+                $(".landingimage." + imageName).stop().fadeTo(200, 0.0);
+
+            });
+
+    });
+
 });
-
-function registerBackgroundImage(imageName) {
-    $("#landingscreen a." + imageName).hover(
-        function() {
-
-            $("#maintitle").css("opacity", "1");
-            //$("#maintitle").css("mix-blend-mode", "exclusion");
-            $(".landingimage." + imageName).stop().fadeTo(200, 1);
-
-        },
-        function() {
-
-            $("#maintitle").css("opacity", "1");
-            //$("#maintitle").css("mix-blend-mode", "normal");
-            $(".landingimage." + imageName).stop().fadeTo(200, 0.0);
-            
-        });
-}
