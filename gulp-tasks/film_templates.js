67369037ffd6f2920dabeda9e70c734907fff522
Jake Coppinger <jake@jakecoppinger.com>
2016-06-20 15:15:08 +1000

    Finished film templating

--- gulp-tasks/film_templates.js
+++ gulp-tasks/film_templates.js
@@ -6,31 +6,25 @@ var runSequence = require('run-sequence');
 var del = require('del');
 var addsrc = require('gulp-add-src');
 
-//var films = require('source/data.json').films;
+var films = require('../source/data.json').films;
 
-var films = [{ "title": "Dark Matter" }];
+function titleToFilename(title) {
+    var newTitle = title.toLowerCase().split(' ').join('_')
+    return newTitle;
+}
 
-gulp.task('cleanFilmTemplates', del.bind(null, ['source/pages/film']));
-
-gulp.task("createFilmTemplates", function() {
-    return gulp.src('source/dummy.json')
-        .pipe(foreach(function(stream, f) {
-            films.forEach(function(film) {
-                stream
-                    .pipe(file(film.title + ".html", '{% extends "film.njk" %}{% set title = "' + film.title + '" %}{% set film = films[title] %}))'))
-                    .pipe(gulp.dest(".tmp/film"));
-            });
-            return stream;
-        }))
-        .pipe(addsrc([".tmp/film/*.html","!**.*/dummy.json"]))
-        .pipe(gulp.dest("source/pages/film"));
+gulp.task('cleanFilm', function() {
+    return del('source/pages/film/*');
 });
 
-
-gulp.task('cleanupFileTemplates', del.bind(null, ['source/pages/film/dummy.json']));
-
-gulp.task("buildFilms", function(callback) {
-    runSequence(
-        ['cleanFilmTemplates'],['createFilmTemplates'],['cleanupFileTemplates'],
-        callback);
+gulp.task("createFilmTemplates", ['cleanFilm'], function() {
+    var stream2 = gulp.src('source/dummy.json')
+        .pipe(foreach(function(stream, f) {
+            for (var film in films) {
+                stream.pipe(file(titleToFilename(film) + ".html", '{% extends "film.njk" %}{% set title = "' + film + '" %}{% set film = films[title] %}))'))
+                    .pipe(gulp.dest("source/pages/film"));
+            }
+            return stream;
+        }))
+    return stream2;
 });
